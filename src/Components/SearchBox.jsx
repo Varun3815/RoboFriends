e25e66214433d11e1d0d697f22d8c17f9aaffd05
Varun3815 <vaarunkot@gmail.com>
2024-02-19 08:21:28 +0530

    updating files

---
+++ src/Components/SearchBox.jsx
@@ -0,0 +1,14 @@
+function SearchBox({ searchField, searchChange }) {
+  return (
+    <div className="pa2">
+      <input
+        className="tc pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="Search Robots"
+        onChange={searchChange}
+      />
+    </div>
+  );
+}
+
+export default SearchBox;
